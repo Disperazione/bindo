0c5074ee66cfca08815bf71cad7db2665d5a4ed3
Raditya Nugra <68360964+RadityaNugra@users.noreply.github.com>
2021-09-21 10:17:51 +0700

    kurang

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,17 +9,11 @@
                 <div class="row">
                     <div class="col-md-8">
                         <h1 class="text-white" data-animation-out="fadeOutRight" data-delay-out="5"
-                            data-duration-in=".3" data-animation-in="fadeInLeft" data-delay-in=".1">Your bright future
-                            is our mission</h1>
+                            data-duration-in=".3" data-animation-in="fadeInLeft" data-delay-in=".1">Lihat Berita
+                        Dengan Mudah</h1>
                         <p class="text-muted mb-4" data-animation-out="fadeOutRight" data-delay-out="5"
-                            data-duration-in=".3" data-animation-in="fadeInLeft" data-delay-in=".4">Lorem ipsum dolor
-                            sit amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor
-                            incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer</p>
-                        <a href="contact.html" class="btn btn-primary" data-animation-out="fadeOutRight"
-                            data-delay-out="5" data-duration-in=".3" data-animation-in="fadeInLeft"
-                            data-delay-in=".7">Apply now</a>
+                            data-duration-in=".3" data-animation-in="fadeInLeft" data-delay-in=".4">
+                        Baca berita dimana saja kapan saja dengan mudah </p>
                     </div>
                 </div>
             </div>
@@ -28,33 +22,10 @@
                 <div class="row">
                     <div class="col-md-8">
                         <h1 class="text-white" data-animation-out="fadeOutUp" data-delay-out="5" data-duration-in=".3"
-                            data-animation-in="fadeInDown" data-delay-in=".1">Your bright future is our mission</h1>
+                            data-animation-in="fadeInDown" data-delay-in=".1">Berita Aktual Dan Faktual</h1>
                         <p class="text-muted mb-4" data-animation-out="fadeOutUp" data-delay-out="5"
-                            data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".4">Lorem ipsum dolor
-                            sit amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor
-                            incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer</p>
-                        <a href="contact.html" class="btn btn-primary" data-animation-out="fadeOutUp" data-delay-out="5"
-                            data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".7">Apply now</a>
-                    </div>
-                </div>
-            </div>
-            <!-- slider item -->
-            <div class="hero-slider-item">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h1 class="text-white" data-animation-out="fadeOutDown" data-delay-out="5" data-duration-in=".3"
-                            data-animation-in="fadeInUp" data-delay-in=".1">Your bright future is our mission</h1>
-                        <p class="text-muted mb-4" data-animation-out="fadeOutDown" data-delay-out="5"
-                            data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".4">Lorem ipsum dolor sit
-                            amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor
-                            incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer</p>
-                        <a href="contact.html" class="btn btn-primary" data-animation-out="fadeOutDown"
-                            data-delay-out="5" data-duration-in=".3" data-animation-in="zoomIn" data-delay-in=".7">Apply
-                            now</a>
+                            data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".4">
+                        Menyediakan berita yang Fakta dan akurat kepada Anda</p>
                     </div>
                 </div>
             </div>
@@ -67,26 +38,18 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-6 order-2 order-md-1">
-                <h3 class="section-title">Salah,Vaksin nusantara mampu hancurkan semua jenis virus Corona dengan Efikasi
-                    100%</h3>
+                <h2 class="section-title">About VISCO</h2>
                 <p>
-                    Tangkapan layar pesan berantai di aplikasi WhatsApp berisi klaim bahwa vaksin Nusantara ampuh 100
-                    persen hancurkan semua jenis virus Corona, beredar di Facebook, pada 6 September 2021.
+                   Didirikan pada tahun 2020,VISCO mempunyai tujuan untuk membantu masyarakat dalam membaca berita yang aktual
+                   dan juga faktual
                 </p>
                 <p>
-                    Dalam tangkapan layar itu, pesan yang tertulis, memuat informasi yang diklaim berasal dari mantan
-                    Menkes Siti Fadilah Supari dengan isi: “Vaksin Nusantara dengan teknologi dendritik diklaim ampuh
-                    100 persen hancurkan semua jenis virus Corona (alpha, beta, delta, delta plus, lambda & jenis virus
-                    lainnya).
-                </p>
-                <p>
-                    Isi pesan berikutnya memuat klaim bahwa tingkat efikasi dan efektivitas sebesar 100 persen, serta
-                    diklaim aman bagi yang memiliki penyakit penyerta (komorbid), anak dan ibu hamil.
+                   Kami juga memberikan kenyamanan pada anda saat membaca berita 
                 </p>
                 {{-- <a href="about.html" class="btn btn-primary-outline">Learn more</a> --}}
             </div>
             <div class="col-md-6 order-1 order-md-2 mb-4 mb-md-0">
-                <img class="img-fluid w-100" src="https://cdn.tmpo.co/data/2021/09/13/id_1050561/1050561_720.jpg"
+                <img class="img-fluid w-100" src="https://d26bwjyd9l0e3m.cloudfront.net/wp-content/uploads/2016/10/situs-berita-startup-featured.png"
                     alt="about image">
             </div>
         </div>
@@ -95,7 +58,7 @@
 <!-- /about us -->
 
 <!-- success story -->
-<section class="section bg-primary" data-background="images/backgrounds/success-story.jpg">
+{{-- <section class="section bg-primary" data-background="images/backgrounds/success-story.jpg">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-sm-4  success-video">
@@ -113,50 +76,14 @@
                         Sehingga klaim bahwa vaksin Nusantara dapat ampuh hancurkan semua jenis virus Corona dengan
                         efikasi dan efektivitas 100 persen tidak memiliki rujukan data ilmiah.
                     </p>
-                    <p>
-                        “Itu omong kosong karena sama sekali tidak ada bukti uji klinis tahap 3,” kata dia kepada Tempo,
-                        Senin 13 September 2021.
-                        Ketua Komisi Nasional Kejadian Ikutan Pasca Imunisasi (Komnas KIPI) Prof Hindra Irawan Satari,
-                        juga menjelaskan bahwa uji klinis vaksin Nusantara hanya pada tahap 1 yang hanya menjangkau
-                        puluhan orang. Vaksin tersebut belum sampai pada uji klinis fase 2 yang melibatkan seratus orang
-                        dan fase 3 terhadap ribuan orang.
-
-                    </p>
                 </div>
             </div>
         </div>
     </div>
-</section>
+</section> --}}
 <!-- /success story -->
 
-<!-- events -->
-<section class="section bg-gray">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md-6 order-2 order-md-1">
-                <h3 class="section-title">Kesimpulan</h3>
-                <p>
-                    Dari pemeriksaan fakta di atas, klaim bahwa vaksin Nusantara ampuh hancurkan semua jenis virus
-                    Corona dengan efektivitas dan efikasi 100 persen adalah keliru. Vaksin Nusantara belum menjalani uji
-                    klinis tahap 3 yang dilakukan terhadap ribuan orang untuk mengetahui tingkat keamanan dan efikasi.
-                </p>
-                <p>
-                    Sel dendritik yang menjadi basis vaksin Nusantara bersifat autologus artinya dari materi yang
-                    digunakan dari diri kita sendiri dan untuk diri kita sendiri, sehingga tidak bisa digunakan untuk
-                    orang lain.
-                </p>
-                {{-- <a href="about.html" class="btn btn-primary-outline">Learn more</a> --}}
-            </div>
-            <div class="col-md-6 order-1 order-md-2 mb-4 mb-md-0">
-                <img class="img-fluid w-100"
-                    src="https://www.lensaindonesia.com/wp-content/uploads/2021/08/vaksin-nusantara.jpg"
-                    alt="about image">
-            </div>
-        </div>
-    </div>
-    </div>
-    <!-- mobile see all button -->
 
-    </div>
-</section>
+
+
 @endsection
